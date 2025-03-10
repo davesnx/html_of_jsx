f4484641e71bfbcb53ff71141d6a214b2ce4125b
David Sancho Moreno <dsnxmoreno@gmail.com>
2025-03-10 17:26:03 +0100

    Push ppx_react

--- ppx/Ppx_extra_attributes.ml
+++ ppx/Ppx_extra_attributes.ml
@@ -1,4 +1,4 @@
-type t = Htmx | Alpinejs | Custom_file of string
+type t = Htmx | Alpinejs | React | Custom_file of string
 
 let reference : t option ref = ref None
 let set flag = reference := Some flag
@@ -8,6 +8,7 @@ let get () = !reference
 let reference_to_string = function
   | Htmx -> "Htmx"
   | Alpinejs -> "Alpine"
+  | React -> "React"
   | Custom_file _ -> "Custom"
 
 let to_string = function None -> "Html" | Some ref -> reference_to_string ref
@@ -16,6 +17,7 @@ let attributes_of_flags = function
   | None -> []
   | Some Htmx -> Ppx_htmx.attributes
   | Some Alpinejs -> Ppx_alpinejs.attributes
+  | Some React -> Ppx_react.attributes
   | Some (Custom_file _) -> (* Not implemented *) []
 
 (* A function that loads the attributes lazy, since we need to derive the list of attributes with the ppx flags. *)
