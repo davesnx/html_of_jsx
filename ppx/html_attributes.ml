009a447aa35619514bd0933e8f4bdf59b0b8b6a1
David Sancho Moreno <dsnxmoreno@gmail.com>
2025-08-04 15:18:58 +0200

    Run 0.26.1 locally

--- ppx/html_attributes.ml
+++ ppx/html_attributes.ml
@@ -7,8 +7,8 @@ type kind =
   | Bool
   | BooleanishString
     (* BooleanishString are attributes that are boolean values but represented as strings on the DOM.
-     https://github.com/facebook/react/blob/a17467e7e2cd8947c595d1834889b5d184459f12/packages/react-dom-bindings/src/server/ReactFizzConfigDOM.js#L1165-L1176
-  *)
+       https://github.com/facebook/react/blob/a17467e7e2cd8947c595d1834889b5d184459f12/packages/react-dom-bindings/src/server/ReactFizzConfigDOM.js#L1165-L1176
+    *)
   | Style
 
 type eventType =
