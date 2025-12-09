81eb0ca994eea245b1576b3adce69cfe762d576b
David Sancho Moreno <dsnxmoreno@gmail.com>
2025-12-09 14:13:08 +0100

    A small refactor on scape

--- lib/JSX.ml
+++ lib/JSX.ml
@@ -6,26 +6,27 @@ let is_self_closing_tag = function
   | _ -> false
 
 let escape buf s =
-  let len = String.length s in
-  let exception Needs_escape of int in
-  try
-    for i = 0 to len - 1 do
+  let length = String.length s in
+  let exception First_char_to_escape of int in
+  match
+    for i = 0 to length - 1 do
       match String.unsafe_get s i with
-      | '&' | '<' | '>' | '\'' | '"' -> raise_notrace (Needs_escape i)
+      | '&' | '<' | '>' | '\'' | '"' -> raise_notrace (First_char_to_escape i)
       | _ -> ()
-    done;
-    Buffer.add_string buf s
-  with Needs_escape first ->
-    if first > 0 then Buffer.add_substring buf s 0 first;
-    for i = first to len - 1 do
-      match String.unsafe_get s i with
-      | '&' -> Buffer.add_string buf "&amp;"
-      | '<' -> Buffer.add_string buf "&lt;"
-      | '>' -> Buffer.add_string buf "&gt;"
-      | '\'' -> Buffer.add_string buf "&apos;"
-      | '"' -> Buffer.add_string buf "&quot;"
-      | c -> Buffer.add_char buf c
     done
+  with
+  | exception First_char_to_escape first ->
+      if first > 0 then Buffer.add_substring buf s 0 first;
+      for i = first to length - 1 do
+        match String.unsafe_get s i with
+        | '&' -> Buffer.add_string buf "&amp;"
+        | '<' -> Buffer.add_string buf "&lt;"
+        | '>' -> Buffer.add_string buf "&gt;"
+        | '\'' -> Buffer.add_string buf "&apos;"
+        | '"' -> Buffer.add_string buf "&quot;"
+        | c -> Buffer.add_char buf c
+      done
+  | _ -> Buffer.add_string buf s
 
 type attribute =
   string * [ `Bool of bool | `Int of int | `Float of float | `String of string ]
